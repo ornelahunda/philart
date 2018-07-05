af0eb0385bb29ae59e3025b10ffbc61a3b207d77
ornelahunda <ornelahunda@gmail.com>
2018-07-05 14:59:50 -0400

    modifing navbar, same for user/nonuser

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,19 +1,57 @@
 
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem , MenuItem, NavDropdown} from 'react-bootstrap';
+import API from '../../utils/API';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem , MenuItem, NavDropdown} from 'react-bootstrap';
+
 import './Navbar.css';
 
-export default class CustomNavbar extends Component {
-  render() {
+export default class MainNavbar extends Component {
+
+state = {
+    user: null
+};
+
+componentDidMount() {
+    this.getUser();
+}
+
+getUser = () => {
+    // api call
+    API.getUser()
+        .then(res => {
+            this.setState(res.data);
+        })
+        .catch(err => console.log(err));
+}
+
+logoutUser = event => {
+    event.preventDefault();
+    API.logoutUser().then(res => {
+        console.log(res.data);
+        if (res.data === true) {
+            this.setState({ user: null });
+        }
+    })
+        .catch(err => console.log(err));
+}
+
+render() {
     return (
-      <Navbar default collapseOnSelect>
-        <Navbar.Header>
+
+           <Navbar default collapseOnSelect>
+           <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">PubArt Philly</Link>
+     
+          {this.state.user ? ( <h5>Welcome {this.state.user.username} </h5> ) : (
+
+            <Link to="/">PubArt Philly</Link> )}
+          
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+
+
         <Navbar.Collapse>
           <Nav pullRight>
             <NavItem eventKey={1} componentClass={Link} href="/" to="/">
@@ -23,28 +61,25 @@ export default class CustomNavbar extends Component {
               Art Near Me
             </NavItem>
 
-     <NavDropdown eventKey={3} title="Register" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1} componentClass={Link} href="/about" to="/login">Login</MenuItem>
-        <MenuItem eventKey={3.2}componentClass={Link} href="/about" to="/register">Register</MenuItem>
-        {/* Logout option */}
- 
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Google Maps</MenuItem>
-      </NavDropdown>
 
 
+     {this.state.user ? (
+     <NavDropdown eventKey={3} title="Account" id="basic-nav-dropdown">         
+     <MenuItem eventKey={3.1} componentClass={Link} href="/collection" to="/collection">Collection</MenuItem>
+     <MenuItem divider />
+     <MenuItem eventKey={3.2} componentClass={Link} href="/" to="/"><a className="btn btn-success" className="text-center"  onClick={this.logoutUser}> Logout </a>
+     </MenuItem>
+     </NavDropdown>
+    ) : (
+     <NavDropdown eventKey={3} title="Account" id="basic-nav-dropdown">     
+     <MenuItem eventKey={3.1} componentClass={Link} href="/about" to="/register">Register</MenuItem> 
+     <MenuItem divider />            
+     <MenuItem eventKey={3.2} componentClass={Link} href="/about" to="/login"> <a className="btn btn-default text-center" className="text-center"> Login </a> </MenuItem>
 
-
-
-
-            {/* <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-              News */}
-
-            {/* </NavItem> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
-}
-
+     </NavDropdown>)}
+  
+    </Nav>
+    </Navbar.Collapse>
+    </Navbar>
+      
+    );}}
